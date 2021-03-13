2cda8865d790880debb65ca0b2677c72d850b741
Luis Cardona <luis.cardona>
2021-03-13 09:52:07 -0500

    feat: add git ignore

--- build/circle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Circle = void 0;
-class Circle {
-    draw() {
-        return 'Circle';
-    }
-}
-exports.Circle = Circle;
--- build/rectangle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Rectangle = void 0;
-class Rectangle {
-    draw() {
-        return 'Rectangle';
-    }
-}
-exports.Rectangle = Rectangle;
